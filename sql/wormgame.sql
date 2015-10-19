4cb272670055237400df9c94264eff6d0ca911bd
juhani.pirinen@edu.karelia.fi <juhani.pirinen@edu.karelia.fi>
2015-10-19 05:02:44 +0300

    Fix database schema (issue found by test case T02)

--- sql/wormgame.sql
+++ sql/wormgame.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.44, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: wormgame
 -- ------------------------------------------------------
--- Server version	5.6.26-log
+-- Server version	5.5.44-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,16 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `wormgame`
+--
+
+/*!40000 DROP DATABASE IF EXISTS `wormgame`*/;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `wormgame` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `wormgame`;
 
 --
 -- Table structure for table `highscores`
@@ -29,17 +39,8 @@ CREATE TABLE `highscores` (
   PRIMARY KEY (`id`),
   KEY `user` (`user`),
   CONSTRAINT `highscores_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `highscores`
---
-
-LOCK TABLES `highscores` WRITE;
-/*!40000 ALTER TABLE `highscores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `highscores` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -50,19 +51,10 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `username` varchar(20) NOT NULL,
-  `password` varchar(20) DEFAULT NULL,
+  `password` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -73,4 +65,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-23 21:16:58
+-- Dump completed on 2015-10-19  4:59:23
